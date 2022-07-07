320558b3aa7cd0b930e271cede77ab106892842b
Anthony Shellman <atshellman@gmail.com>
2022-07-06 21:14:14 -0600

    Add constuctor and methods

--- src/Turn.js
+++ src/Turn.js
@@ -1,9 +1,35 @@
-const Card = require('../src/Card');
-
 class Turn {
   constructor(guess, card) {
     this.guess = guess;
     this.card = card;
+  }
+
+  returnGuess() {
+    let guess = this.guess;
+    return guess;
+  }
+
+  returnCard() {
+    let card = this.card;
+    return card;
+  }
+
+  evaluateGuess() {
+    let correctAnswer = this.card['correctAnswer'];
+    if (this.guess === correctAnswer) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  giveFeedback() {
+    let evaluation = this.evaluateGuess()
+    if (evaluation === true) {
+      return 'Correct!'
+    } else {
+      return 'Incorrect!'
+    }
   }
 }
 
